686d0a3cf4ca0f516781fdc765da503923750b8b
yuno <qvxz0395@gmail.com>
2021-12-14 17:20:16 +0900

    移動平均データを作れた．

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime as dt
 df = pd.read_pickle('datas.pkl')
+dataPeriod = 30# minute
+meanPeriod = [5,25,75]# day
 
+for i in meanPeriod:
+	df["mean"+str(i)] = df["open"].rolling(int(i*24*60/dataPeriod),center=True).mean()
 print(df)
 
-plt.plot(df.index,df["open"])
+plt.plot(df.index,df["open"],label = "open")
+for i in meanPeriod:
+	plt.plot(df.index,df["mean"+str(i)],label ="mean"+str(i))
+plt.legend()
 plt.show()
