6bf11c19aaf42664066d90d975b16a34dcb3a69e
y-yoga <y-yoga>
2021-12-19 03:15:09 +0900

    ボリンジャーバンドプロット

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ dataPeriod = 30# minute
 meanPeriod = [5,25,75]# day
 for i in meanPeriod:
 	df["mean"+str(i)] = df["open"].rolling(int(i*24*60/dataPeriod),center=True).mean()
+	df["std"+str(i)] = df["open"].rolling(int(i*24*60/dataPeriod),center=True).std()# sample standard deviation
+	df["bband+2σ"+str(i)] = df["mean"+str(i)] + (2*df["std"+str(i)])# Bollinger Band
+	df["bband-2σ"+str(i)] = df["mean"+str(i)] - (2*df["std"+str(i)])# ref: https://www.moneypartners.co.jp/support/tech/bolb.html
 
 
+
+print(df)
 
 plt.plot(df.index,df["open"],label = "open")
 for i in meanPeriod:
 	plt.plot(df.index,df["mean"+str(i)],label ="mean"+str(i))
+	plt.plot(df.index,df["bband+2σ"+str(i)],label = "bband+2σ"+str(i))
+	plt.plot(df.index,df["bband-2σ"+str(i)],label = "bband-2σ"+str(i))
 plt.legend()
 plt.show()
