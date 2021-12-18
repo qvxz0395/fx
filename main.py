4bde0ef96a6c666fed0ecc8f4eac4670ce5ac5c3
y-yoga <y-yoga>
2021-12-19 02:19:06 +0900

    プログラムを見やすく変更

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime as dt
-df = pd.read_pickle('datas.pkl')
-dataPeriod = 30# minute
-meanPeriod = [5,25,75]# day
 
+df = pd.read_pickle('datas.pkl')# read data
+dataPeriod = 30# minute
+
+# calc moving average
+meanPeriod = [5,25,75]# day
 for i in meanPeriod:
 	df["mean"+str(i)] = df["open"].rolling(int(i*24*60/dataPeriod),center=True).mean()
-print(df)
+
+
 
 plt.plot(df.index,df["open"],label = "open")
 for i in meanPeriod:
