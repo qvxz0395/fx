01a1b78bf6622a22674463a56dd74607bac519b0
y-yoga <y-yoga>
2021-12-19 03:37:49 +0900

    生データにサンプル点平均値を追加した。単純にopen high low closeを平均しているだけなので、将来的にはもっと頭のいい方法で平均値を出したい。

--- arrangeData.py
+++ arrangeData.py
@@ -6,6 +6,7 @@ data = pd.read_csv("USDJPY_M30_201310240000_202112140800.csv",sep="\t")
 data = data.rename(columns={"<DATE>":"date","<TIME>":"time","<OPEN>":"open","<HIGH>":"high","<LOW>":"low","<CLOSE>":"close","<TICKVOL>":"tickvol","<VOL>":"vol","<SPREAD>":"spread"}) # rename column name
 
 data["datetime"] = data.apply(lambda row:dt.datetime.strptime(row['date'] +' '+ row['time'], '%Y.%m.%d %H:%M:%S'),axis=1)# add datetime column
+data["mean"] = data.filter(items = ["open","high","low","close"]).mean(axis = "columns")#add mean column
 data = data.drop(columns=["date","time"])# delete columns
 
 print(data)
